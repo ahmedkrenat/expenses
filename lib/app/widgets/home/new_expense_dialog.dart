352f6ec50df04607175c849146a6138cbcff6155
ahmed <ahmed-krenat@live.com>
2024-08-28 19:05:06 +0300

    select custom dateTime for new expenses

--- lib/app/widgets/home/new_expense_dialog.dart
+++ lib/app/widgets/home/new_expense_dialog.dart
@@ -17,14 +17,17 @@ class NewExpenseDialog extends StatefulWidget {
 class _NewExpenseDialogState extends State<NewExpenseDialog> {
   late TextEditingController _descController;
   late TextEditingController _amountController;
+  final TextEditingController _dateTimeController = TextEditingController();
   bool _isDescEmpty = false;
   bool _isAmountEmpty = false;
+  DateTime _selectedDateTime = DateTime.now();
 
   @override
   void initState() {
     super.initState();
     _descController = TextEditingController(text: '');
     _amountController = TextEditingController(text: '');
+    _dateTimeController.text = _selectedDateTime.toLocal().toString().split('.')[0];
   }
 
   InputDecoration _buildInputDecoration(String label, bool isEmpty) {
@@ -63,12 +66,41 @@ class _NewExpenseDialogState extends State<NewExpenseDialog> {
     Expense expense = Expense(id: '',
         description: _descController.text,
         amount: double.parse(_amountController.text),
-        timestamp: Timestamp.now(),
+        timestamp: Timestamp.fromDate(_selectedDateTime ),
         userId: widget.user.uid
     );
 
     await CloudFirestore.addExpense(expense);
     Navigator.of(context).pop();
+  }
+
+  Future<void> _selectDateTime(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: _selectedDateTime,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null) {
+      final TimeOfDay? pickedTime = await showTimePicker(
+        context: context,
+        initialTime: TimeOfDay.fromDateTime(_selectedDateTime),
+      );
+
+      if (pickedTime != null) {
+        setState(() {
+          _selectedDateTime = DateTime(
+            pickedDate.year,
+            pickedDate.month,
+            pickedDate.day,
+            pickedTime.hour,
+            pickedTime.minute,
+          );
+          _dateTimeController.text = _selectedDateTime.toLocal().toString().split('.')[0];
+        });
+      }
+    }
   }
 
 
@@ -88,6 +120,18 @@ class _NewExpenseDialogState extends State<NewExpenseDialog> {
             controller: _amountController,
             decoration: _buildInputDecoration(Constants.amount, _isAmountEmpty),
             keyboardType: TextInputType.numberWithOptions(decimal: true),
+          ),
+          SizedBox(height: 10),
+          TextField(
+            controller: _dateTimeController,
+            decoration: InputDecoration(
+              labelText: 'Date & Time',
+              suffixIcon: IconButton(
+                icon: Icon(Icons.calendar_today),
+                onPressed: () => _selectDateTime(context),
+              ),
+            ),
+            readOnly: true,
           ),
         ],
       ),
